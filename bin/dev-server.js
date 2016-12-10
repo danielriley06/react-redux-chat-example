4670335ea846dad02cfdb68f15cda78a07971d66
Daniel Riley <driley3@gmail.com>
2016-12-09 22:55:27 -0500

    Update dev-server to use HTTP module to allow socketIO connections

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -1,6 +1,12 @@
+const http = require('http')
 const project = require('../config/project.config')
-const server = require('../server/main')
+const appServerConfig = require('../server/main')
 const debug = require('debug')('app:bin:dev-server')
+const SocketIo = require('socket.io')
+
+const server = http.createServer(appServerConfig)
+const io = new SocketIo(server, {path: '/api/chat'})
+const socketEvents = require('../server/socketEvents')(io)
 
 server.listen(project.server_port)
 debug(`Server is now running at http://localhost:${project.server_port}.`)
